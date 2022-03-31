7c3a4a7f1fb5949e8df9adb01368aa7793899ba9
lythanhtung11 <tungthanh.ly@dxc.com>
2022-03-31 16:05:50 +0700

    Add project files.

---
+++ BookStore/Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using BookStore.Data;
+using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController: ControllerBase
+    {
+        private readonly MockUserRepo _repo = new MockUserRepo();
+
+        public UsersController()
+        {
+
+        }
+
+        [HttpGet]
+        public ActionResult <IEnumerable<User>> GetAllUsers()
+        {
+            return Ok(_repo.GetAllUser());
+        }
+    }
+}
---
+++ BookStore/Data/MockUserRepo.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class MockUserRepo : IUserRepo
+    {
+        public IEnumerable<User> GetAllUser()
+        {
+            var users = new List<User>
+            {
+                new User {Id=1,Username="tung",Password="123",Token="123"},
+                new User {Id=2,Username="tung",Password="123",Token="123"},
+                new User {Id=3,Username="tung",Password="123",Token="123"}
+            };
+            return users;
+        }
+    }
+}
